f8e5835670e04d4b920720844f69c48746b5a4e8
kyle <42522603+ktg5@users.noreply.github.com>
2026-06-26 11:28:36 -0700

    okay, what does this say?

--- buildStaging.ts
+++ buildStaging.ts
@@ -70,8 +70,8 @@ export async function copyDir(sourceDir: string, newDir: string):Promise<void> {
 export async function moveDirs(ogdir: string, dist: string) {
     console.log(fs.readdirSync(config.stagingHtmlDir));
     ogdir = `${config.stagingHtmlDir}/${ogdir}`;
-    console.log(fs.readdirSync(ogdir));
     dist = `${config.stagingHtmlDir}/${dist}`;
+    console.log(fs.readdirSync(dist));
 
     const ogExists = fs.existsSync(ogdir);
     if (!ogExists) {
